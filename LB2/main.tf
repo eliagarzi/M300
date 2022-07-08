03f8b4374aff7e2837b935c389aa09651edcbb47
Elia Garzi <62818267+eliagarzi@users.noreply.github.com>
2022-07-08 09:27:23 +0200

    Terraform HCL Script für Kubernetes und Registry done

---
+++ LB2/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "kubernetes_group" {
+  name     = "kubernetes-group"
+  location = "Switzerland North"
+}
+
+resource "azurerm_container_registry" "registry-zh1" {
+  name                = "registry-zh1"
+  resource_group_name = azurerm_resource_group.kubernetes_group.name
+  location            = azurerm_resource_group.kubernetes_group.location
+  sku                 = "Basic"
+}
+
+resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
+  depends_on = [
+    azurerm_resource_group.kubernetes_group,
+    azurerm_container_registry.registry-zh1
+  ]
+
+  name                = "kubernetes-zh1"
+  location            = azurerm_resource_group.kubernetes_group.location
+  resource_group_name = azurerm_resource_group.kubernetes_group.name
+  dns_prefix          = "kubernetes-zh1"
+
+  default_node_pool {
+    name       = "kubernetesnodepool"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssgined"
+  }    
+}
