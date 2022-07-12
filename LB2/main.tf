a28d40e25f75fe89a5f84b8c6ebe1eb30cfb5387
Elia Garzi <62818267+eliagarzi@users.noreply.github.com>
2022-07-12 08:13:30 +0200

    Update main.tf

--- LB2/main.tf
+++ LB2/main.tf
@@ -16,8 +16,8 @@ resource "azurerm_resource_group" "kubernetes_group" {
   location = "Switzerland North"
 }
 
-resource "azurerm_container_registry" "registry-zh1" {
-  name                = "registry-zh1"
+resource "azurerm_container_registry" "garseb" {
+  name                = "garseb"
   resource_group_name = azurerm_resource_group.kubernetes_group.name
   location            = azurerm_resource_group.kubernetes_group.location
   sku                 = "Basic"
@@ -35,12 +35,12 @@ resource "azurerm_kubernetes_cluster" "kubernetes_cluster" {
   dns_prefix          = "kubernetes-zh1"
 
   default_node_pool {
-    name       = "kubernetesnodepool"
+    name       = "knodepool"
     node_count = 1
     vm_size    = "Standard_D2_v2"
   }
 
   identity {
-    type = "SystemAssgined"
+    type = "SystemAssigned"
   }    
 }
